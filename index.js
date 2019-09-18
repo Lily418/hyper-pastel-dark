f218731a9a7c94cf436c690081d0ac99f68d64ff
Lily <lily@helixcentre.com>
2019-09-18 17:22:37 +0100

    Make testing colors used by jest a bit stronger

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ exports.decorateConfig = (config) => {
     cursorColor: '#D0CFC8',
     colors: {
       black: '#373737',
-      red: '#DF9291',
-      green: '#B5D1B0',
-      yellow: '#F0D7A4',
+      red: '#EB6E6C',
+      green: '#99EB8A',
+      yellow: '#EDE84C',
       blue: '#93AFCF',
       magenta: '#D1B8D4',
       cyan: '#9FD2D3',
